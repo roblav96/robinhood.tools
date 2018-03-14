22137d6bf5b845dba0831e107327eea9c67aa7ba
Robert Laverty <roblav96@gmail.com>
2018-03-14 00:10:23 -0400

    NATS x 38,439 ops/sec ±2.64% (74 runs sampled)

--- src/server/services/benchmarks.ts
+++ src/server/services/benchmarks.ts
@@ -6,7 +6,7 @@ import * as _ from 'lodash'
 import * as common from '../../common'
 
 import * as Nats from 'nats'
-import * as ws from 'uws'
+import * as Uws from 'uws'
 import * as prettyhrtime from 'pretty-hrtime'
 import * as microtime from 'microtime'
 import * as Benchmark from 'benchmark'
@@ -16,13 +16,10 @@ import * as ee3 from 'eventemitter3'
 
 if (process.MASTER) {
 
-	const wss = new ws.Server({ port: (process.PORT - 1) })
-	wss.on('error', function(error) { console.error('wss Error >', error) })
-
-	wss.on('connection', function(client) {
-		client.on('message', function(message: string) {
-			client.send(message)
-		})
+	const nats = Nats.connect(process.PORT)
+	nats.on('error', function(error) { console.error('nats Error >', error) })
+	nats.subscribe('message', function(data) {
+		nats.publish('message', data)
 	})
 
 }
@@ -35,35 +32,98 @@ if (process.WORKER) {
 
 
 
-	const socket = new ws('ws://localhost:' + (process.PORT - 1))
-	socket.on('close', function(code, message) { console.warn('close', code, message) })
-	socket.on('error', function(error) { console.error('socket Error >', error) })
+	const nats = Nats.connect(process.PORT)
+	nats.on('error', function(error) { console.error('nats Error >', error) })
 
-	const wsEE3 = new ee3.EventEmitter()
-	socket.on('message', function(data) {
-		wsEE3.emit('message', data)
+	const natsEE3 = new ee3.EventEmitter()
+	nats.subscribe('message', function(data) {
+		natsEE3.emit('message', data)
 	})
 
-	suite.add('uWebSockets', {
+	suite.add('NATS', {
 		defer: true,
 		fn: function(next) {
-			socket.send(Date.now().toString())
-			wsEE3.once('message', function(data) {
+			nats.publish('message', Date.now().toString())
+			natsEE3.once('message', function(data) {
 				next.resolve(data)
 			})
 		},
 	})
 
 	suite.on('cycle', function(event) {
-		// console.log('COMPLETE >', event.target.toString())
+		nats.close()
 		console.info('cycle')
 		eyes.inspect(event.target.toString())
 	})
 
-	socket.on('open', function() {
+	nats.on('connect', function() {
 		console.warn('start')
-		suite.run({ async: true, name: 'buttface?' })
+		suite.run({ async: true })
 	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// const wss = new Uws.Server({ port: (process.PORT - 1) })
+	// wss.on('error', function(error) { console.error('wss Error >', error) })
+	// wss.on('connection', function(client) {
+	// 	client.on('message', function(message: string) {
+	// 		client.send(message)
+	// 	})
+	// })
+
+
+
+	// const socket = new Uws('ws://localhost:' + (process.PORT - 1))
+	// socket.on('close', function(code, message) { console.warn('close', code, message) })
+	// socket.on('error', function(error) { console.error('socket Error >', error) })
+
+	// const uwsEE3 = new ee3.EventEmitter()
+	// socket.on('message', function(data) {
+	// 	uwsEE3.emit('message', data)
+	// })
+
+	// suite.add('uWebSockets', {
+	// 	defer: true,
+	// 	fn: function(next) {
+	// 		socket.send(Date.now().toString())
+	// 		uwsEE3.once('message', function(data) {
+	// 			next.resolve(data)
+	// 		})
+	// 	},
+	// })
+
+	// suite.on('cycle', function(event) {
+	// 	socket.close()
+	// 	console.info('cycle')
+	// 	eyes.inspect(event.target.toString())
+	// })
+
+	// socket.on('open', function() {
+	// 	console.warn('start')
+	// 	suite.run({ async: true })
+	// })
 
 
 
