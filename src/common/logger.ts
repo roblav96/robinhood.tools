f590b6d7b41b1524440a175b7089d35d5027c607
Robert Laverty <roblav96@gmail.com>
2018-03-29 13:18:12 -0400

    stripped well

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,11 +1,13 @@
 // 
 
 import chalk from 'chalk'
+import * as strip from 'cli-color/strip'
 import * as eyes from 'eyes'
 import * as util from 'util'
 import * as _ from 'lodash'
 import * as Pino from 'pino'
 import * as moment from 'moment'
+import * as stream from 'stream'
 import * as stacktrace from 'stack-trace'
 import * as pretty from './pretty'
 
@@ -15,8 +17,7 @@ const logger = Pino({
 	// name: process.NAME,
 	prettyPrint: {
 		formatter: (function(log, config) {
-			// console.log('log ->', log)
-			console.time('formatter')
+			// console.time('formatter')
 
 			let frames = stacktrace.get()
 			let index = frames.findIndex(function(frame) {
@@ -27,22 +28,25 @@ const logger = Pino({
 			log.sourceUrl = log.sourceUrl.replace(process.cwd() + '/', '')
 			log.method = logger.levels.labels[log.level]
 
-			// console.info(`${chalk.bold.redBright('████  formatter -> log  ████')}\n`, eyes.stringify(log))
-			console.log(`${chalk.bold.blueBright('████  formatter -> log  ████')}\n`, log)
+			// console.log(`${chalk.bold.blueBright('████  formatter -> log  ████')}\n`, log)
 
 			if (log.msg) {
 				process.stdout.write(`\n\n${chalk.bold.red(`process.stdout.write log.msg ->`)}\n\n${log.msg}\n\n`)
+				log.msg = strip(log.msg)
 			} else {
 				let i: number, len = 11
 				for (i = 0; i < len; i++) {
 					if (log[i]) {
 						process.stdout.write(`\n\n${chalk.bold.red(`process.stdout.write ${i} ->`)}\n\n${log[i]}\n\n`)
+						log[i] = strip(log[i])
 					}
 				}
 			}
 
-			console.timeEnd('formatter')
-			return '\nformatter -> return string'
+			console.log(`${chalk.bold.blueBright('████  formatter -> log  ████')}\n`, log)
+
+			// console.timeEnd('formatter')
+			return '\n\nformatter -> return string'
 
 		} as Pino.PrettyFormatter) as any,
 	},
@@ -63,23 +67,32 @@ for (i = 0; i < len; i++) {
 	let proxy = proxies[i]
 	Object.assign(global._console, { [proxy]: global.console[proxy] })
 	Object.assign(global.console, {
-		[proxy](...args) {
-			// console.log('args ->', args)
-			logger[proxy].call(logger, [...args.map(util.inspect as any)])
-			// logger[proxy].apply(logger, _.map(args, _.unary(wtf)))
-			// logger[proxy].apply(logger, ...args.map(wtf))
-			// logger[proxy].apply(logger, [eyes.stringify.apply(eyes.stringify, args)])
-			// logger[proxy].apply(logger, [util.format.call(util.format, Array.prototype.slice.call(args))])
-			// logger[proxy].apply(logger, [args])
-			// logger[proxy].apply(logger, [Array.prototype.slice.call(args).map(wtf)])
-			// logger[proxy].apply(logger, [util.inspect.call(util.inspect, args)])
-			// logger[proxy].apply(logger, [[...args].map(util.format)])
-			// logger[proxy].call(logger, [util.format.call(util.format, Array.prototype.slice.call(args))])
-			// logger[proxy].call(logger, [...args])
-			// logger[proxy].apply(logger, ...args.map(util.format))
-			if (process.env.INSPECTING) global._console[proxy](...args);
+		[proxy](...args: string[]) {
+			logger[proxy].call(logger, [...args.map(eyes.stringify)])
 			// logger[proxy].call(logger, [...args.map(util.inspect as any)])
-			// logger[proxy].apply(logger, [util.format.apply(util.format, Array.prototype.slice.call(args))])
+			// logger[proxy].call(logger, [...args])
+			if (process.env.INSPECTING) global._console[proxy](...args);
+			// // console.log('args ->', args)
+			// // let wtf = util.format.call(util.format, [[...args]])
+			// let wtf = args.map(util.format)
+			// console.log('wtf ->', wtf)
+			// console.log('Array.isArray(wtf) ->', Array.isArray(wtf))
+			// logger[proxy].apply(logger, [...wtf])
+			// // logger[proxy].call(logger, util.format.call(util.format, [...args])
+			// // logger[proxy].apply(logger, _.map(args, _.unary(wtf)))
+			// // logger[proxy].apply(logger, ...args.map(wtf))
+			// // logger[proxy].apply(logger, [eyes.stringify.apply(eyes.stringify, args)])
+			// // logger[proxy].apply(logger, [util.format.call(util.format, Array.prototype.slice.call(args))])
+			// // logger[proxy].apply(logger, [args])
+			// // logger[proxy].apply(logger, [Array.prototype.slice.call(args).map(wtf)])
+			// // logger[proxy].apply(logger, [util.inspect.call(util.inspect, args)])
+			// // logger[proxy].apply(logger, [[...args].map(util.format)])
+			// // logger[proxy].call(logger, [util.format.call(util.format, Array.prototype.slice.call(args))])
+			// // logger[proxy].call(logger, [...args])
+			// // logger[proxy].apply(logger, ...args.map(util.format))
+			// if (process.env.INSPECTING) global._console[proxy](...args);
+			// // logger[proxy].call(logger, [...args.map(util.inspect as any)])
+			// // logger[proxy].apply(logger, [util.format.apply(util.format, Array.prototype.slice.call(args))])
 		},
 	})
 }
@@ -92,9 +105,9 @@ import * as boom from 'boom'
 let error = boom.internal(`A horrible internal error has occured!!!`)
 
 function testLoggerError() {
-	console.time('console.error')
+	// console.time('console.error')
 	console.error('boom.internal Error ->', error, 'error.message ->', `"${error.message}"`, 'error.stack ->', error.stack)
-	console.timeEnd('console.error')
+	// console.timeEnd('console.error')
 }
 testLoggerError()
 
