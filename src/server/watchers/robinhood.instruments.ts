3d890df74b3271a64168d03fec8a7e4395e3e969
Robert Laverty <roblav96@gmail.com>
2018-04-05 20:50:36 -0400

    syncall to instruments

--- src/server/watchers/robinhood.instruments.ts
+++ src/server/watchers/robinhood.instruments.ts
@@ -102,3 +102,5 @@ async function chunkSymbols() {
 }
 
 
+
+
--- src/server/watchers/webull.tickers.ts
+++ src/server/watchers/webull.tickers.ts
@@ -23,7 +23,7 @@ if (process.MASTER) {
 		console.error('readyTickers Error ->', error)
 	}).finally(function() {
 		console.warn('emit -> webull.tickers.ready')
-		// radio.emit('webull.tickers.ready')
+		radio.emit('webull.tickers.ready')
 	})
 }
 
@@ -42,8 +42,17 @@ async function readyTickers() {
 
 }
 
+
+
 async function syncTickers() {
 	await syncAll()
+	
+	let symbols = await robinhood.getAllSymbols()
+	console.log('symbols ->', console.inspect(symbols))
+
+	// let proms = core.array.create(process.INSTANCES).map(i => radio.pEvent('workerTickers.' + i))
+	// radio.emit('workerTickers')
+	// await Promise.all(proms)
 
 	console.info('syncTickers -> done')
 
@@ -52,17 +61,16 @@ async function syncTickers() {
 
 
 async function syncAll() {
-	let all = await Promise.all([
+	let all = _.flatten(await Promise.all([
 		http.get('https://securitiesapi.webull.com/api/securities/market/tabs/v2/6/cards/8', {
 			query: { pageSize: 999999 },
 		}),
 		http.get('https://securitiesapi.webull.com/api/securities/market/tabs/v2/6/cards/13', {
 			query: { pageSize: 999999 },
-		})
-	])// as Webull.Ticker[]
+		}),
+	])) as Webull.Ticker[]
 
 	_.remove(all, v => Array.isArray(v.disSymbol.match(/\W+/)))
-	console.log('all.length ->', console.inspect(all.length))
 
 	let grouped = _.groupBy(all, 'disSymbol' as keyof Webull.Ticker)
 	let coms = Object.keys(grouped).map(function(symbol) {
@@ -71,6 +79,72 @@ async function syncAll() {
 	await redis.main.coms(coms)
 
 	console.info('syncAll -> done')
+
+}
+
+
+
+async function syncTicker(symbol: string) {
+	// console.log('symbol ->', console.inspect(symbol), console.inspect(progress))
+
+	let instrument = await redis.main.hgetall(`${redis.RH.INSTRUMENTS}:${symbol}`) as Robinhood.Instrument
+	core.fix(instrument)
+	// console.warn('instrument ->', console.inspect(_.pick(instrument, ['symbol', 'name', 'simple_name', 'good', 'country', 'acronym'] as KeysOf<Robinhood.Instrument>)))
+
+	await clock.pEvent('250ms')
+	let response = await http.get('https://infoapi.webull.com/api/search/tickers2', {
+		query: { keys: symbol },
+	}) as Webull.API.Paginated<Webull.Ticker>
+	if (!Array.isArray(response.list)) return;
+
+	let tags = core.string.tags(instrument.name)
+	let results = response.list.filter(function(v) {
+		return v && v.disSymbol.indexOf(symbol) == 0
+	}).map(function(v) {
+		return Object.assign(v, {
+			match: _.intersection(tags, core.string.tags(v.tickerName)).length,
+		})
+	})
+
+	let result = results.find(v => v.match > 0 && v.disExchangeCode.indexOf(instrument.acronym) == 0 && v.regionAlias == instrument.country)
+	if (!result) result = results.find(v => v.match > 0 && v.disExchangeCode.indexOf(instrument.acronym) == 0);
+	if (!result) result = results.find(v => v.disExchangeCode.indexOf(instrument.acronym) == 0);
+	if (!result) result = results.find(v => v.match > 0);
+	if (!result) result = results.find(v => v.disSymbol == symbol && results.length == 1);
+	if (!result) {
+		if (instrument.good) {
+			console.error('!result ->', console.inspect(instrument), 'response.list ->', console.inspect(response.list))
+		}
+		return
+	}
+
+	let ticker = _.omit(result, ['match']) as Webull.Ticker
+	// console.log('ticker ->', console.inspect(_.pick(ticker, ['disSymbol', 'tickerName', 'tinyName', 'regionAlias', 'disExchangeCode'] as KeysOf<Webull.Ticker>)))
+	await redis.main.hmset(`${redis.WB.TICKERS}:${symbol}`, ticker)
+
+	// await clock.pEvent('250ms')
+
+}
+
+
+
+if (process.WORKER) {
+
+	radio.on('workerTickers', async function workerTickers() {
+		let symbols = await robinhood.getSymbols()
+		console.log('symbols.length ->', console.inspect(symbols.length))
+
+		await pAll(symbols.map((v, i) => function() {
+			let prog = core.number.round((i / symbols.length) * 100)
+			console.log(console.inspect(prog), 'symbol ->', console.inspect(v))
+			return syncTicker(v)
+		}), { concurrency: 1 })
+		// await syncTicker('AAIT')
+
+		console.info('workerTickers -> done')
+		radio.emit('workerTickers.' + process.INSTANCE)
+
+	})
 
 }
 
@@ -103,65 +177,6 @@ async function syncAll() {
 // 	}).finally(function() {
 // 		radio.emit('webull.tickers.done')
 // 	})
-// }
-
-
-
-// async function syncTickers() {
-// 	let symbols = await robinhood.getSymbols()
-// 	console.log('symbols.length ->', console.inspect(symbols.length))
-
-// 	await pAll(symbols.map((v, i) => function() {
-// 		let prog = core.number.round((i / symbols.length) * 100)
-// 		console.log(console.inspect(prog), 'symbol ->', console.inspect(v))
-// 		return syncTicker(v)
-// 	}), { concurrency: 1 })
-// 	// await syncTicker('AAIT')
-
-// 	console.info('syncTickers -> done')
-
-// }
-
-// async function syncTicker(symbol: string) {
-// 	// console.log('symbol ->', console.inspect(symbol), console.inspect(progress))
-
-// 	let instrument = await redis.main.hgetall(`${redis.RH.INSTRUMENTS}:${symbol}`) as Robinhood.Instrument
-// 	core.fix(instrument)
-// 	// console.warn('instrument ->', console.inspect(_.pick(instrument, ['symbol', 'name', 'simple_name', 'good', 'country', 'acronym'] as KeysOf<Robinhood.Instrument>)))
-
-// 	await clock.pEvent('250ms')
-// 	let response = await http.get('https://infoapi.webull.com/api/search/tickers2', {
-// 		query: { keys: symbol },
-// 	}) as Webull.API.Paginated<Webull.Ticker>
-// 	if (!Array.isArray(response.list)) return;
-
-// 	let tags = core.string.tags(instrument.name)
-// 	let results = response.list.filter(function(v) {
-// 		return v && v.disSymbol.indexOf(symbol) == 0
-// 	}).map(function(v) {
-// 		return Object.assign(v, {
-// 			match: _.intersection(tags, core.string.tags(v.tickerName)).length,
-// 		})
-// 	})
-
-// 	let result = results.find(v => v.match > 0 && v.disExchangeCode.indexOf(instrument.acronym) == 0 && v.regionAlias == instrument.country)
-// 	if (!result) result = results.find(v => v.match > 0 && v.disExchangeCode.indexOf(instrument.acronym) == 0);
-// 	if (!result) result = results.find(v => v.disExchangeCode.indexOf(instrument.acronym) == 0);
-// 	if (!result) result = results.find(v => v.match > 0);
-// 	if (!result) result = results.find(v => v.disSymbol == symbol && results.length == 1);
-// 	if (!result) {
-// 		if (instrument.good) {
-// 			console.error('!result ->', console.inspect(instrument), 'response.list ->', console.inspect(response.list))
-// 		}
-// 		return
-// 	}
-
-// 	let ticker = _.omit(result, ['match']) as Webull.Ticker
-// 	// console.log('ticker ->', console.inspect(_.pick(ticker, ['disSymbol', 'tickerName', 'tinyName', 'regionAlias', 'disExchangeCode'] as KeysOf<Webull.Ticker>)))
-// 	await redis.main.hmset(`${redis.WB.TICKERS}:${symbol}`, ticker)
-
-// 	// await clock.pEvent('250ms')
-
 // }
 
 
