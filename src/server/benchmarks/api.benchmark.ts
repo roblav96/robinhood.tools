6aa44c3eb032b88ff9c0839c74ffa6a4b7d7ce85
Robert Laverty <roblav96@gmail.com>
2018-04-29 06:37:05 -0400

    start to actually benchmark

--- src/server/benchmarks/api.benchmark.ts
+++ src/server/benchmarks/api.benchmark.ts
@@ -2,6 +2,7 @@
 
 import * as execa from 'execa'
 import * as onexit from 'exit-hook'
+import * as url from 'url'
 import * as turbo from 'turbo-http'
 import * as pAll from 'p-all'
 import * as Table from 'cli-table2'
@@ -12,13 +13,16 @@ import * as wrk from './wrk'
 
 
 async function run(url: string) {
-	console.log('run ->', url)
+	console.log('benchmark ->', url)
 	let cli = await execa('wrk', ['-t1', '-c100', '-d3s', url, '--latency'])
 	return wrk.parse(cli.stdout)
 }
 
 async function start() {
 
+	let descs = [
+		'Barebones Turbo HTTP',
+	]
 	let urls = []
 	core.array.create(+process.env.INSTANCES).forEach(function(i) {
 		if (i == 0) return;
@@ -26,24 +30,22 @@ async function start() {
 	})
 	// urls.push(`http://${process.env.HOST}:${process.env.IPORT}`)
 	// urls.push(`http://${process.env.HOST}:${process.env.PORT}`)
-	// urls.push(`http://${process.env.HOST}:${process.env.PORT}/api/hello`)
-	// urls.push(`http://${process.env.DOMAIN}/api/hello`)
+	urls.push(`http://${process.env.HOST}:${process.env.PORT}/api/hello`)
+	urls.push(`http://${process.env.DOMAIN}/api/hello`)
 
 	let results = await pAll(urls.map(v => () => run(v)), { concurrency: 1 })
-	// console.log('results ->', JSON.stringify(results, null, 4))
 
-	let descs = [
-		'res.end()',
-	]
 	let table = new Table({
-		head: ['Description', 'Address', 'Req/sec', 'Data/sec', 'Latency'],
+		// head: ['Description', 'Address', 'Req/sec', 'Data/sec', 'Latency'],
+		head: ['Address', 'Req/sec', 'Data/sec', 'Latency'],
 		colAligns: ['left', 'left', 'right', 'right', 'right'],
 		style: { head: ['bold', 'magenta'] },
 	}) as any[]
 	results.forEach(function(v, i) {
+		let parsed = url.parse(urls[i])
 		table.push([
-			descs[i] || i,
-			urls[i],
+			// descs[i],
+			parsed.host.concat(parsed.path),
 			pretty.formatNumber(v.requests.rate),
 			pretty.formatNumber(v.transfer.rate, 2),
 			pretty.formatNumber(v.latency.avg, 2),
@@ -61,28 +63,9 @@ const INSTANCE = +process.env.INSTANCE
 const BUFFER = Buffer.from(JSON.stringify({ hello: 'world' }))
 const server = turbo.createServer(function handler(req, res) {
 	res.end()
-	// if (INSTANCE == 1) {
-	// 	res.setHeader('Content-Length', BUFFER.length)
-	// 	res.write(BUFFER)
-	// }
-	// if (INSTANCE == 2) {
-	// 	let buffer = Buffer.from(JSON.stringify({ hello: 'world' }))
-	// 	res.setHeader('Content-Length', buffer.length)
-	// 	res.write(buffer)
-	// }
-	// if (INSTANCE == 3) {
-	// 	res.end(BUFFER)
-	// }
-	// if (INSTANCE == 4) {
-	// 	let buffer = Buffer.from(JSON.stringify({ hello: 'world' }))
-	// 	res.end(buffer)
-	// }
-	// if (INSTANCE == 5) {
-	// 	res.end()
-	// }
 })
 server.listen(+process.env.IPORT, process.env.HOST, function onlisten() {
-	console.info('server listening ->', process.env.HOST + ':' + process.env.IPORT)
+	console.info('listening ->', process.env.HOST + ':' + process.env.IPORT)
 	if (!process.env.PRIMARY) return;
 	start().catch(function(error: Error & execa.ExecaReturns) {
 		console.error(`'${error.cmd}' ->`, error)
