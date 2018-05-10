ba17d0badcce6999ab8110eb723043cac64e07a6
Robert Laverty <roblav96@gmail.com>
2018-05-10 19:05:49 -0400

    rm those

--- src/server/api/api.ts
+++ src/server/api/api.ts
@@ -13,11 +13,4 @@ import './validator.hook'
 import * as utils from '../adapters/utils'
 utils.requireDir(__dirname, __filename, v => v.endsWith('.api.ts'))
 
-// import './security.api'
-// import './socket.api'
-// import './hours.api'
-// import './search.api'
-// import './quotes.api'
-// import './symbols.api'
-
 
