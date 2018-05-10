833321fa7ace92d7c2dcec262bad6a9bf788e5ac
Robert Laverty <roblav96@gmail.com>
2018-05-10 19:05:39 -0400

    forgot hooks

--- src/server/api/api.ts
+++ src/server/api/api.ts
@@ -6,9 +6,9 @@ import './polka'
 import './polka.request'
 import './polka.response'
 
-// import './cors.hook'
-// import './security.hook'
-// import './validator.hook'
+import './cors.hook'
+import './security.hook'
+import './validator.hook'
 
 import * as utils from '../adapters/utils'
 utils.requireDir(__dirname, __filename, v => v.endsWith('.api.ts'))
