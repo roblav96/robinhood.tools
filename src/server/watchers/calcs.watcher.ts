7e678fffa244c47d8eda5c7d9c6b911b0905651b
Robert Laverty <roblav96@gmail.com>
2018-05-11 16:52:41 -0400

    emitters

--- src/server/watchers/calcs.watcher.ts
+++ src/server/watchers/calcs.watcher.ts
@@ -4,6 +4,16 @@ import emitter from './quotes.watcher'
 
 
 
+emitter.on('fsymbols', function(fsymbols: Dict<number>, hubdata: Symbols.OnSymbolsData) {
 
+})
+
+emitter.on('data', function(topic: number, wbquote: Webull.Quote) {
+
+})
+
+emitter.on('toquote', function(topic: number, wbquote: Webull.Quote) {
+
+})
 
 
