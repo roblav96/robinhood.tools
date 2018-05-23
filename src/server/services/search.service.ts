783fae8b2658695397cc1eed2b95cc54d6273a83
Robert Laverty <roblav96@gmail.com>
2018-05-22 20:00:41 -0400

    wait wut?

--- src/server/services/search.service.ts
+++ src/server/services/search.service.ts
@@ -2,23 +2,22 @@
 
 import '../main'
 import * as lunr from 'lunr'
-import * as schedule from 'node-schedule'
 import * as rkeys from '../../common/rkeys'
-import * as pretty from '../../common/pretty'
 import * as pandora from '../adapters/pandora'
 import * as redis from '../adapters/redis'
-import * as http from '../adapters/http'
-import * as socket from '../adapters/socket'
-import * as hours from '../adapters/hours'
+import * as utils from '../adapters/utils'
 
 
 
 pandora.on('symbols.reset', onSymbols)
 pandora.once('symbols.ready', onSymbols)
-pandora.broadcast({}, 'symbols.start')
+pandora.broadcast({}, 'symbols.starts')
 
 async function onSymbols(hubmsg: Pandora.HubMessage) {
-	console.log(`hubmsg ->`, hubmsg)
+	// console.log(`hubmsg ->`, hubmsg)
+	
+	let symbols = await utils.getSymbols()
+	
 }
 
 
