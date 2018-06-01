e18a5047b5c78d1ca17aee3fb4bc0d47f3942e6b
Robert Laverty <roblav96@gmail.com>
2018-06-01 17:22:43 -0400

    more charts

--- src/client/routes/symbol/symbol.ticker.ts
+++ src/client/routes/symbol/symbol.ticker.ts
@@ -47,14 +47,14 @@ export default class extends Mixins(VMixin) {
 		let bidask = { bid: { price: 0, size: 0 }, ask: { price: 0, size: 0 } }
 		if (Object.keys(this.all.quote).length == 0) return bidask;
 		{
-			let max = this.all.quote.askPrice - this.all.quote.bidPrice
-			bidask.bid.price = core.calc.slider(this.all.quote.price - this.all.quote.bidPrice, 0, max)
-			bidask.ask.price = core.calc.slider(this.all.quote.askPrice - this.all.quote.price, 0, max)
+			let max = this.all.quote.ask - this.all.quote.bid
+			bidask.bid.price = core.calc.slider(this.all.quote.price - this.all.quote.bid, 0, max)
+			bidask.ask.price = core.calc.slider(this.all.quote.ask - this.all.quote.price, 0, max)
 		}
 		{
-			let max = this.all.quote.bidLot + this.all.quote.askLot
-			bidask.bid.size = core.calc.slider(this.all.quote.bidLot, 0, max)
-			bidask.ask.size = core.calc.slider(this.all.quote.askLot, 0, max)
+			let max = this.all.quote.bids + this.all.quote.asks
+			bidask.bid.size = core.calc.slider(this.all.quote.bids, 0, max)
+			bidask.ask.size = core.calc.slider(this.all.quote.asks, 0, max)
 		}
 		return bidask
 	}
