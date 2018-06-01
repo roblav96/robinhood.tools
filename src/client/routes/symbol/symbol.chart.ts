10616df8c4f9ed7f6febc6dcf43f266d1779db8a
Robert Laverty <roblav96@gmail.com>
2018-06-01 16:49:49 -0400

    echarts

--- src/client/routes/symbol/symbol.chart.ts
+++ src/client/routes/symbol/symbol.chart.ts
@@ -3,6 +3,8 @@
 import * as Vts from 'vue-property-decorator'
 import { mixins as Mixins } from 'vue-class-component'
 import Vue from 'vue'
+import * as echarts from 'echarts'
+import * as ecStat from 'echarts-stat'
 import * as core from '@/common/core'
 import * as rkeys from '@/common/rkeys'
 import * as http from '@/client/adapters/http'
@@ -12,7 +14,9 @@ import * as http from '@/client/adapters/http'
 @Vts.Component
 export default class extends Vue {
 	
-	
+	created() {
+		console.log(`echarts ->`, echarts)
+	}
 
 }
 
